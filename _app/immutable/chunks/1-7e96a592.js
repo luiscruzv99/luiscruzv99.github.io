1beabb3ddf92fd073785297feec95d15a0f924e2
Luis C <luiscruzv99@gmail.com>
2023-02-01 20:27:12 +0000

    hotfix

---
+++ _app/immutable/chunks/1-7e96a592.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-615cd93b.js";export{t as component};
---
+++ _app/immutable/chunks/3-b35f122d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/bdays/natalia/_page.svelte-98a7df79.js";export{t as component};
